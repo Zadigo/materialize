90575eb90851e765a94ad8296bc57a2f20a2d714
John <pendenquejohn@gmail.com>
2019-05-20 01:20:28 +0200

    Improvements on app

--- test_app/wsgi.py
+++ test_app/wsgi.py
@@ -6,13 +6,17 @@ BASE_DIR = os.path.basename(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-# app.config['MEDIA'] = os.path.join(BASE_DIR, 'media')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MEDIA'] = os.path.join(BASE_DIR, 'media')
 
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('hero/hero.html')
+
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    return render_template('other/contact.html')
 
 
 if __name__ == "__main__":
