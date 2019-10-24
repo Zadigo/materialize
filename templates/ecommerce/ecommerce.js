8e54793db47dc207daf0ca63376ee9c791288941
John <pendenquejohn@gmail.com>
2019-10-24 15:24:41 +0200

    Mobile pages

--- templates/ecommerce/ecommerce.js
+++ templates/ecommerce/ecommerce.js
@@ -1,30 +1,30 @@
-$(document).ready(function () {
-    var ecommerce = (function() {
-        var sideImages = $(".side-images img")
-        var mainImage = $(".main-image img")
+// $(document).ready(function () {
+//     var ecommerce = (function() {
+//         var sideImages = $(".side-images img")
+//         var mainImage = $(".main-image img")
 
-        var changeImage = function(e) {
-            // Subtitute main image to one
-            // of the sides
-            var imageURL = $(this).attr("src")
-            mainImage.attr({src: imageURL})
-        }
+//         var changeImage = function(e) {
+//             // Subtitute main image to one
+//             // of the sides
+//             var imageURL = $(this).attr("src")
+//             mainImage.attr({src: imageURL})
+//         }
 
-        var addEvent = function() {
-            // Select each images
-            sideImages.each(image => {
-                $(sideImages[image]).on("click", changeImage)
-            })
-        }
+//         var addEvent = function() {
+//             // Select each images
+//             sideImages.each(image => {
+//                 $(sideImages[image]).on("click", changeImage)
+//             })
+//         }
 
-        var init = function() {
-            addEvent()
-        }
+//         var init = function() {
+//             addEvent()
+//         }
 
-        return {
-            init
-        }
-    })()
+//         return {
+//             init
+//         }
+//     })()
 
-    ecommerce.init()
-});
+//     ecommerce.init()
+// });
