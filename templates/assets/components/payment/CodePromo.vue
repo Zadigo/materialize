4ecdd1a206cdff6bb82f6b7c626fb48aa040e691
John Pendenque <pendenquejohn@gmail.com>
2020-02-19 11:29:32 +0100

    Refactoring repository #3

--- templates/assets/components/payment/CodePromo.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="card">
-        <div class="card-content">
-            <h3>Code promotionel</h3>
-        </div>
-    </div>
-</template>
--- templates/assets/components/payment/Livraison.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="card">
-        <div class="card-content">
-            <h3>Pays de livraison</h3>
-            <button class="btn">Modifier</button>
-        </div>
-    </div>
-</template>
