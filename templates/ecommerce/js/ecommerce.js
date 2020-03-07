512d8b87c4f0d17ad0dbefd9d9f5657e5e3f6fb7
John Pendenque <pendenquejohn@gmail.com>
2020-03-07 23:13:13 +0100

    Improved overall feel on products page

--- templates/ecommerce/js/ecommerce.js
+++ templates/ecommerce/js/ecommerce.js
@@ -1,3 +1,51 @@
+var databaseproducts = [
+    { 
+        id: 1, 
+        name: "Jupe de soie",
+        exclusive: true,
+        discount_price: 46,
+        discount_pct: 25,
+        price_ht: 151,
+        images: [{ "main_image": true, image_url: "./assets/sac_a_dos_marron2.jpg" }] 
+    },
+    { 
+        id: 2,
+        name: "Jupe de coton",
+        exclusive: false,
+        discount_pct: 0,
+        price_ht: 0,
+        price_ht: 106,
+        images: [{ "main_image": true, image_url: "./assets/sac_crossbody_similicuir_black.jpg" }] 
+    },
+    { 
+        id: 3,
+        name: "Jupe en cardigan",
+        exclusive: false,
+        discount_pct: 0,
+        price_ht: 0,
+        price_ht: 450,
+        images: [{ "main_image": true, image_url: "./assets/small_tote_bag_red.jpg" }] 
+    },
+    { 
+        id: 4,
+        name: "Jupe de polen",
+        exclusive: true,
+        discount_pct: 0,
+        price_ht: 0,
+        price_ht: 753,
+        images: [{ "main_image": true, image_url: "./assets/tote_bag_luxe_rouge.jpg" }] 
+    },
+    { 
+        id: 5,
+        name: "Jupe de végétal",
+        exclusive: false,
+        discount_pct: 0,
+        price_ht: 0,
+        price_ht: 150,
+        images: [{ "main_image": true, image_url: "./assets/sac_a_dos_marron2.jpg" }] 
+    },
+]
+
 var filterbar = {
     template: "\
         <div class='wrapper'>\
@@ -25,22 +73,41 @@ var cards = {
     props: ["products"],
     template:"\
         <div class='grid' id='products'>\
-                <div v-for='product in products' :key='product.id' class='product'>\
-                    <a href='product.html'>\
+                <div v-for='(product, index) in products' :key='product.pk' class='product'>\
+                    <a @click='pushdatalayer(index, product)' :href='builddetailurl(product)' id='btn_product_details'>\
                         <div class='image'>\
-                            <img src='../images/image3.jpg' alt='image3'>\
-                            <div v-show='product.exclusive' class='banner'>Exclusive</div>\
+                            <img :src='getmainimage(product.images)' :alt='product.slug'>\
+                            <div v-if='product.discount_price > 0' class='discount-pct grey darken-2'>{{ product.discount_pct|pricepct }}</div>\
+                            <div v-show='product.exclusive' class='banner grey lighten-3'>Exclusive</div>\
                         </div>\
                         <div class='details'>\
                             <div class='title'>{{ product.name }}</div>\
                             <div class='price-details'>\
-                                <div class='price'>{{ product.price }}€</div>\
+                                <div class='price'>{{ product.price_ht }}€</div>\
                             </div>\
                         </div>\
                     </a>\
                 </div>\
         </div>\
-    "
+    ",
+    methods: {
+        builddetailurl: function(product) { return "product.html" },
+        pushdatalayer: function(index, product) {},
+        getmainimage: function(images) {
+            var mainimage = ""
+            images.forEach(image => {
+                if (image.main_image === true) {
+                    mainimage = image.image_url
+                }
+            })
+            return mainimage
+        }
+    },
+    filters: {
+        pricepct: function(pct) {
+            return "-" + pct + "%"
+        }
+    }
 }
 
 var app = new Vue({
@@ -48,13 +115,7 @@ var app = new Vue({
     components: {cards, filterbar},
     data() {
         return {
-            products: [
-                {id: 1, name: "Jupe de soie", exclusive: true, price: 151},
-                {id: 2, name: "Jupe de coton", exclusive: false, price: 106},
-                {id: 3, name: "Jupe en cardigan", exclusive: false, price: 450},
-                {id: 4, name: "Jupe de polen", exclusive: true, price: 753},
-                {id: 5, name: "Jupe de végétal", exclusive: false, price: 150},
-            ],
+            products: databaseproducts,
             productfilters: {priceorder: "initial"}
         }
     },
@@ -67,19 +128,24 @@ var app = new Vue({
         },
         oderprices() {
             var self = this
+            // Copy the products array in order to keep
+            // the original product's state when we sort
+            // the products or do something else with them
+            var copiedproducts = [...self.$data.products]
+
             if (self.$data.productfilters.priceorder === "initial") {
                 return self.$data.products
             }
             
             if (self.$data.productfilters.priceorder === "croissant") {
-                return self.$data.products.sort((a, b) => {
-                    return a.price - b.price
+                return copiedproducts.sort((a, b) => {
+                    return a.price_ht - b.price_ht
                 })
             }
 
             if (self.$data.productfilters.priceorder === "decroissant") {
-                return self.$data.products.sort((a, b) => {
-                    return b.price - a.price
+                return copiedproducts.sort((a, b) => {
+                    return b.price_ht - a.price_ht
                 })
             }
 
